a5c4ea34e54d9ab8f81cb88f865ac026a95efa14
Lucas Silva <ssilvalucas2004@gmail.com>
2023-02-17 16:20:03 -0300

    Criando classes no tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    './src/index.html',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-facebook': '#1877F2',
+        'gray-facebook': '#F0F2F5',
+        'green-facebook': '#36A420',
+      },
+      spacing: {
+        '580': '580px',
+        '380': '380px',
+      }
+    },
   },
   plugins: [],
 }
